622c85f6b57b84e991586bf32129efb7f085d895
Lisa <programacaolisa@gmail.com>
2026-09-15 14:55:15 -0300

    chore: initialize NormaEdu project

---
+++ src/main/java/com/lisarios/normaedu/NormaeduApplication.java
@@ -0,0 +1,13 @@
+package com.lisarios.normaedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormaeduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NormaeduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lisarios/normaedu/NormaeduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisarios.normaedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NormaeduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
